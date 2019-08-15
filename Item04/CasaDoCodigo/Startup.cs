7d1a2d7914244f8413ac58725b1178c1091a5660
marcelooliveira <mclricardo@gmail.com>
2019-08-15 13:53:25 -0300

    Item04 - início

---
+++ Item04/CasaDoCodigo/Startup.cs
@@ -0,0 +1,103 @@
+﻿using CasaDoCodigo.Repositories;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace CasaDoCodigo
+{
+    public class Startup
+    {
+        private readonly ILoggerFactory _loggerFactory;
+
+        public Startup(ILoggerFactory loggerFactory,
+            IConfiguration configuration)
+        {
+            _loggerFactory = loggerFactory;
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddLogging(loggingBuilder => loggingBuilder.AddSerilog(dispose: true));
+            services.AddMvc();
+            services.AddDistributedMemoryCache();
+            services.AddSession();
+
+            string connectionString = Configuration.GetConnectionString("Default");
+
+            services.AddDbContext<ApplicationContext>(options =>
+                options.UseSqlServer(connectionString)
+            );
+
+            services.AddTransient<IDataService, DataService>();
+            services.AddTransient<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddTransient<IHttpHelper, HttpHelper>();
+            services.AddTransient<IProdutoRepository, ProdutoRepository>();
+            services.AddTransient<IPedidoRepository, PedidoRepository>();
+            services.AddTransient<ICadastroRepository, CadastroRepository>();
+
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+            services.AddAuthentication();
+        }
+
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env,
+            IServiceProvider serviceProvider)
+        {
+            _loggerFactory.AddSerilog();
+
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseAuthentication();
+            app.UseSession();
+            app.UseMvc(routes =>
+            {
+                routes.MapAreaRoute(
+                    name: "AreaCatalogo",
+                    areaName: "Catalogo",
+                    template: "Catalogo/{controller=Home}/{action=Index}/{pesquisa?}");
+
+                routes.MapAreaRoute(
+                    name: "AreaCarrinho",
+                    areaName: "Carrinho",
+                    template: "Carrinho/{controller=Home}/{action=Index}/{codigo?}");
+
+                routes.MapAreaRoute(
+                    name: "AreaCadastro",
+                    areaName: "Cadastro",
+                    template: "Cadastro/{controller=Home}/{action=Index}");
+
+                routes.MapAreaRoute(
+                    name: "AreaPedido",
+                    areaName: "Pedido",
+                    template: "Pedido/{controller=Home}/{action=Index}");
+
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{codigo?}");
+            });
+
+            var dataService = serviceProvider.GetRequiredService<IDataService>();
+            dataService.InicializaDBAsync(serviceProvider).Wait();
+        }
+    }
+}
